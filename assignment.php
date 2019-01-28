94cc0efc9d6246c20ea9e244562eb68331af9ec9
Devanté <GitEntity@users.noreply.github.com>
2019-01-27 21:40:28 -0500

    db connection setup

--- assignment.php
+++ assignment.php
@@ -1,6 +1,35 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"
+          integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w"
+          crossorigin="anonymous">
+    <style>
+        .button-success,
+        .button-error,
+        .button-warning,
+        .button-secondary {
+            color: white;
+            border-radius: 4px;
+            text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+        }
+
+        .button-success {
+            background: rgb(28, 184, 65); /* this is a green */
+        }
+
+        .button-error {
+            background: rgb(202, 60, 60); /* this is a maroon */
+        }
+
+        .button-warning {
+            background: rgb(223, 117, 20); /* this is an orange */
+        }
+
+        .button-secondary {
+            background: rgb(66, 184, 221); /* this is a light blue */
+        }
+    </style>
     <link rel="stylesheet" type="text/css" href="/cloud-computing/style.css">
     <title>Virtual Car Garage</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
@@ -23,15 +52,15 @@
     <tr>
         <td>
             <form>
-                <input type="submit" value="Remove from Garage">
-                <input type="submit" value="Compare Cars">
-                <input type="submit" value="Delete Garage">
+                <button class="button-warning pure-button">Warning Button</button>
+                <button class="button-success pure-button">Success Button</button>
+                <button class="button-error pure-button">Error Button</button>
             </form>
         </td>
         <td>
             <form>
-                <input type="submit" value="Add to Garage">
-                <input type="submit" value="Create New Garage">
+                <button class="button-secondary pure-button">Secondary Button</button>
+                <button class="button-success pure-button">Success Button</button>
             </form>
         </td>
     </tr>
