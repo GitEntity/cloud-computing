0090cbf85d551592dd21c6a7a0357aafee0e6198
Devanté <GitEntity@users.noreply.github.com>
2019-01-29 03:07:00 -0500

    added Chart.js

--- carchart.js
+++ carchart.js
@@ -25,31 +25,17 @@ $(document).ready(function () {
             }
 
             var chartdata = {
-                labels: power,
+                labels: carName,
                 datasets: [
                     {
-                        labels: 'Power (HP)',
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(200, 200, 200, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(200, 200, 200, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
+                        label: 'Power (HP)',
+                        backgroundColor: 'rgba(200, 200, 200, 0.75)',
+                        borderColor: 'rgba(200, 200, 200, 0.75)',
+                        hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                        hoverBorderColor: 'rgba(200, 200, 200, 0.75)',
+                        data: power
                     }
+                ]
             };
             var ctx = $("#myCanvas");
 
